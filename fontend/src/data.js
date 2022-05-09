911a084898591cad73b37fdf9c47c697a4b33454
yasinarafatpro <yasinarafat1194@gmail.com>
2022-05-10 00:54:44 +0600

    product cart

--- fontend/src/data.js
+++ fontend/src/data.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const data= {
     products:[
         {
@@ -11,7 +9,8 @@ const data= {
             brand:'Nike',
             rating:4.5,
             reviews:10,
-            description:'High Quality Product'
+            description:'High Quality Product',
+            countInStock:0
         },
         {
             id:'2',
@@ -22,7 +21,20 @@ const data= {
             brand:'Nike',
             rating:4.0,
             reviews:10,
-            description:'High Quality Product'
+            description:'High Quality Product',
+            countInStock:1
+        },
+        {
+            id:'3',
+            name:'Hike Shart',
+            catagory:'Shart',
+            image:'./images/im1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:3.5,
+            reviews:20,
+            description:'High Quality Product',
+            countInStock:4
         }
     ]
 }
--- fontend/src/screen/ProductScreen.js
+++ fontend/src/screen/ProductScreen.js
@@ -1,7 +1,59 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Rating from '../component/Rating';
+import data from '../data'
 
-export const ProductScreen = () => {
+export const ProductScreen = (props) => {
+  const params = useParams();
+  const { id: productId } = params;
+  const product = data.products.find((x) => x.id === productId)
+  if (!product) {
+    return <div>Product Not Found</div>
+  }
   return (
-    <div>ProductScreen</div>
+    <div>
+      <Link to='/'>back to home</Link>
+      <div className='row top'>
+        <div className='col-2'>
+          <img className='large' src={product.image} alt={product.name}></img>
+        </div>
+        <div className='col-1'>
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              <Rating rating={product.rating} Reviews={product.reviews}></Rating>
+            </li>
+            <li>Price:{product.price} Tk</li>
+            <li>Description:<p>{product.description}</p></li>
+          </ul>
+        </div>
+        <div className='col-1'>
+          <div className='card card-body'>
+            <ul>
+              <li>
+                <div className='row'>
+                  <div>Price :</div>
+                  <div className='price'>{product.price} Tk</div>
+                </div>
+              </li>
+              <li>
+                <div className='row'>
+                  <div>Status:</div>
+                  <div>
+                    {product.countInStock > 0 ?
+                      (<span className='success'>In Stock</span>) : (<span className='error'>Out Of Stock!</span>)}
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className='primary block' onClick={(e)=>console.log('yes')}>Add to Cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
